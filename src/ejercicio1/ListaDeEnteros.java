33422cded516a210c99f5c7562781c5e6d9008fb
Santiago Alessandri <san.lt.ss@gmail.com>
2011-03-19 18:19:38 -0300

    Cambiado los metodos de package a publicos

--- src/ejercicio1/ListaDeEnteros.java
+++ src/ejercicio1/ListaDeEnteros.java
@@ -4,15 +4,15 @@ public abstract class ListaDeEnteros {
 	
 	protected int tamanio;
 	
-	abstract void comenzar();
-	abstract void proximo();
-	abstract Integer elemento();
-	abstract Integer elemento(int pos);
-	abstract boolean agregar(Integer elem, int pos);
-	abstract boolean eliminar();
-	abstract boolean eliminar(int pos);
-	abstract boolean incluye(Integer elem);
-	abstract int tamanio();
-	abstract boolean fin();
+	abstract public void comenzar();
+	abstract public void proximo();
+	abstract public Integer elemento();
+	abstract public Integer elemento(int pos);
+	abstract public boolean agregar(Integer elem, int pos);
+	abstract public boolean eliminar();
+	abstract public boolean eliminar(int pos);
+	abstract public boolean incluye(Integer elem);
+	abstract public int tamanio();
+	abstract public boolean fin();
 
 }
